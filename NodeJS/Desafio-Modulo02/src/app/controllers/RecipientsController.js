ed7c4d14078bd2f0c1b73fe09206186b5c33783f
PedroZenere <pedrinhozenere99@gmail.com>
2020-05-25 13:02:00 -0400

    Finalizando o desafio, falta autenticação

---
+++ NodeJS/Desafio-Modulo02/src/app/controllers/RecipientsController.js
@@ -0,0 +1,44 @@
+import * as Yup from 'yup'
+import Recipients from '../models/Recipients'
+
+class RecipientsController {
+    async store(req, res) {
+        const schema = Yup.object().shape({
+            nome: Yup.string().required(),
+            rua: Yup.string().required(),
+            numero: Yup.number().required(),
+            complemento: Yup.string().required(),
+            estado: Yup.string().required(),
+            cidade: Yup.string().required(),
+            cep: Yup.string().min(9).max(9),
+        })
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation error.' })
+        }
+
+        const {
+            id,
+            nome,
+            rua,
+            numero,
+            complemento,
+            estado,
+            cidade,
+            cep,
+        } = await Recipients.create(req.body)
+
+        return res.status(200).json({
+            id,
+            nome,
+            rua,
+            numero,
+            complemento,
+            estado,
+            cidade,
+            cep,
+        })
+    }
+}
+
+export default new RecipientsController()
--- NodeJS/Desafio-Modulo02/src/routes.js
+++ NodeJS/Desafio-Modulo02/src/routes.js
@@ -1,9 +1,16 @@
+import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
+import RecipientsController from './app/controllers/RecipientsController'
+
 const { Router } = require('express')
 
 const routes = new Router()
 
-routes.get('/', (req, res) => {
-    res.json({ message: 'teste' })
-})
+routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store)
+
+// midleware de autenticação
+
+routes.post('/recipients', RecipientsController.store)
 
 module.exports = routes
