fb7a9f8862036c8d00c9182563e7be62b35a1345
PedroZenere <pedrinhozenere99@gmail.com>
2020-06-13 22:55:22 -0400

    Finalizando primeiro projeto com ReactJS

--- ReactJS/modulo05/src/Pages/Main/index.js
+++ ReactJS/modulo05/src/Pages/Main/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 
 class Main extends Component {
   constructor() {
@@ -13,6 +16,24 @@ class Main extends Component {
       repositories: [],
       loading: false,
     };
+  }
+
+  // Carrega os dados do LocalStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // Salva os dados no LocalStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
   }
 
   handleInputChange = (e) => {
@@ -26,7 +47,13 @@ class Main extends Component {
 
     const { newRepo, repositories } = this.state;
 
-    const response = await api.get(`/repos/${newRepo}`);
+    let response;
+
+    try {
+      response = await api.get(`/repos/${newRepo}`);
+    } catch (err) {
+      alert('Repository not found');
+    }
 
     const data = {
       name: response.data.full_name,
@@ -39,8 +66,16 @@ class Main extends Component {
     });
   };
 
+  handleDeleteOption = (repository) => {
+    const { repositories } = this.state;
+
+    this.setState({
+      repositories: repositories.filter((r) => r.name !== repository),
+    });
+  };
+
   render() {
-    const { newRepo, loading } = this.state;
+    const { newRepo, repositories, loading } = this.state;
 
     return (
       <Container>
@@ -65,6 +100,23 @@ class Main extends Component {
             )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map((repository) => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <button
+                type="button"
+                onClick={() => this.handleDeleteOption(repository.name)}
+              >
+                <FiTrash2 size={14} color="#a8a8b3" />
+              </button>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
